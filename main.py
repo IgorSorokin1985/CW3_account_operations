1c5f0629480d78cd089a49a866ae2061acf4b3b0
IgorSorokin1985 <igorsorokin1985@gmail.com>
2023-08-07 17:23:37 +0300

    add functions

---
+++ main.py
@@ -0,0 +1,5 @@
+from utils.utils import load_data, last_five_operations, creat_message
+
+def main():
+    pass
+
---
+++ utils/utils.py
@@ -0,0 +1,15 @@
+import json
+
+FILENAME = 'operations.json'
+
+def load_file(filename):
+    pass
+
+def load_data():
+    pass
+
+def last_five_operations():
+    pass
+
+def creat_message():
+    pass
