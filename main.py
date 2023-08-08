11d8f6e71ba8eefb130fa88e958df4687697b8e8
IgorSorokin1985 <igorsorokin1985@gmail.com>
2023-08-08 09:35:39 +0300

    add number in main

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from utils.utils import load_data, last_five_operations, creat_message
+from utils.utils import load_data, last_five_operations, creat_message, NUMBER_LAST_OPERATIONS
 
 def main():
     last_operations = last_five_operations()
-    for item in range(5):
+    for item in range(NUMBER_LAST_OPERATIONS):
         print(creat_message(last_operations[item]))
 
 main()
