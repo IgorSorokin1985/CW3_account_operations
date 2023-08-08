a8cde9a5527a8e7d3ebf0fbdb32064ad88968d74
IgorSorokin1985 <igorsorokin1985@gmail.com>
2023-08-08 16:15:59 +0300

    fix mistake

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from utils.utils import load_data, last_five_operations, creat_message
+from utils.utils import load_data, last_five_operations, creat_message, NUMBER_LAST_OPERATIONS, FILENAME
 
 def main():
-    last_operations = last_five_operations()
-    for item in range(5):
+    '''
+    Main function. Get last five operations and print messages.
+    :return: None
+    '''
+    operations = load_data(FILENAME)
+    last_operations = last_five_operations(operations)
+    for item in range(NUMBER_LAST_OPERATIONS):
         print(creat_message(last_operations[item]))
-    pass
 
 main()
